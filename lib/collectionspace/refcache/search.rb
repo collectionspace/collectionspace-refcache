8c9635d9e8195847a3d5210ae4a154093af46b78
Mark Cooper <mark.c.cooper@outlook.com>
2020-06-18 17:18:26 -0700

    Apply default sort and and add more auths

--- lib/collectionspace/refcache/search.rb
+++ lib/collectionspace/refcache/search.rb
@@ -10,7 +10,7 @@ module CollectionSpace
         path = build_path(type, subtype)
         field = @search_identifiers ? 'shortIdentfier' : build_term(type)
         criteria = build_criteria(path, type, field, value)
-        response = @client.search(criteria) # { 'sortBy' => 'collectionspace_core:updatedAt' }
+        response = @client.search(criteria, sortBy: 'collectionspace_core:updatedAt DESC')
         total = response.parsed['abstract_common_list']['totalItems'].to_i
         return nil if total.zero?
 
@@ -43,13 +43,22 @@ module CollectionSpace
         response.parsed['abstract_common_list']['list_item']
       end
 
+      # rubocop:disable Metrics/MethodLength
       def schema(type)
         {
+          'citationauthorities' => { ns: 'citations', term: "citation#{TERM_SUFFIX}" },
+          'conceptauthorities' => { ns: 'concepts', term: "concept#{TERM_SUFFIX}" },
+          'locationauthorities' => { ns: 'locations', term: "loc#{TERM_SUFFIX}" },
+          'materialauthorities' => { ns: 'materials', term: "material#{TERM_SUFFIX}" },
+          'orgauthorities' => { ns: 'organizations', term: "org#{TERM_SUFFIX}" },
           'personauthorities' => { ns: 'persons', term: "person#{TERM_SUFFIX}" },
           'placeauthorities' => { ns: 'places', term: "place#{TERM_SUFFIX}" },
-          'vocabularies' => { ns: 'vocabularyitems', term: 'displayName' }
+          'taxonomyauthority' => { ns: 'taxon', term: "place#{TERM_SUFFIX}" },
+          'vocabularies' => { ns: 'vocabularyitems', term: 'displayName' },
+          'workauthorities' => { ns: 'works', term: "work#{TERM_SUFFIX}" }
         }.fetch(type)
       end
+      # rubocop:enable Metrics/MethodLength
     end
   end
 end
